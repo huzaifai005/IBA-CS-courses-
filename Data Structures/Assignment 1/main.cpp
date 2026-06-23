bbd58c1c261d91b3daafa0a682da5bd6f21fda76
Huzaifa Iqbal <huzaifaiqb005@gmail.com>
2026-06-23 17:11:57 +0500

    Delete Data Structures/Assignment 1 directory

--- Data Structures/Assignment 1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include "Percolation(quick-union).hpp"
-#include "PercolationStats.hpp"
-#include "stopwatch.hpp"
-
-int main() {
-    Percolation::test();
-    PercolationStats::test();
-}
